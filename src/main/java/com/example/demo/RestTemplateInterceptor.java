1171921de3866751a37284caf8238a7134dac68b
Emil Nielsen <emilqz@gmail.com>
2021-03-23 15:03:49 +0100

    Manuel B3 propagation seems to work

--- src/main/java/com/example/demo/RestTemplateInterceptor.java
+++ src/main/java/com/example/demo/RestTemplateInterceptor.java
@@ -1,22 +1,61 @@
 package com.example.demo;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapSetter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static io.opentelemetry.api.GlobalOpenTelemetry.getPropagators;
+
+@Component
 public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
+
+    @Autowired
+    private Tracer tracer;
+
+    private static final TextMapSetter<HttpRequest> setter = (carrier, key, value) -> carrier.getHeaders().set(key, value);
 
     @Override
     public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
         HttpHeaders headers = httpRequest.getHeaders();
+
         System.out.println("HEADERS_BEFORE" + headers);
-        ClientHttpResponse response = clientHttpRequestExecution.execute(httpRequest, bytes);
-        HttpHeaders headers1 = response.getHeaders();
-        System.out.println("HEADERS_AFTER" + headers1);
-        return response;
+        String spanName = httpRequest.getMethodValue() +  " " + httpRequest.getURI().toString();
+        if (tracer == null){
+            System.out.println("tracer is null");
+        } else {
+            System.out.println("trace is not null");
+        }
+        SpanBuilder spanBuilder = tracer.spanBuilder(spanName);
+        Span currentSpan = spanBuilder.setSpanKind(SpanKind.CLIENT).startSpan();
+
+        try (Scope scope = currentSpan.makeCurrent()) {
+            getPropagators().getTextMapPropagator().inject(Context.current(), httpRequest, setter);
+            HttpHeaders headers1 = httpRequest.getHeaders();
+            System.out.println("HEADERS_AFTER" + headers1);
+            ClientHttpResponse response = clientHttpRequestExecution.execute(httpRequest, bytes);
+            return response;
+        } finally {
+            currentSpan.end();
+        }
+
+//        HttpHeaders headers = httpRequest.getHeaders();
+//        System.out.println("HEADERS_BEFORE" + headers);
+//        ClientHttpResponse response = clientHttpRequestExecution.execute(httpRequest, bytes);
+//        HttpHeaders headers1 = response.getHeaders();
+//        System.out.println("HEADERS_AFTER" + headers1);
+//        return response;
     }
 }
--- src/main/java/com/example/demo/ReviewApplication.java
+++ src/main/java/com/example/demo/ReviewApplication.java
@@ -1,29 +1,46 @@
 package com.example.demo;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @SpringBootApplication
 public class ReviewApplication {
 
+	@Autowired
+	private RestTemplateInterceptor restTemplateInterceptor;
+
 	@Bean
 	public RestTemplate getRestTemplate(){
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.setInterceptors(Collections.singletonList(new RestTemplateInterceptor()
-		));
+		restTemplate.setInterceptors(Collections.singletonList(restTemplateInterceptor));
 		return restTemplate;
+	}
+
+	private static final String tracerName = "reviewsSpan";
+
+	@Bean
+	public Tracer getTracer() {
+		Tracer tracer = GlobalOpenTelemetry.getTracer(tracerName);
+		System.out.println("The tracer is " + tracer);
+		SpanProcessor logProcessor = SimpleSpanProcessor.create(new LoggingSpanExporter());
+		SdkTracerProvider tracerProvider = SdkTracerProvider.builder().addSpanProcessor(logProcessor).build();
+		OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
+		return tracer;
 	}
 
 	public static void main(String[] args) {
